7f717242f36269b0ffeb00d3f8e6a20d6b9b2924
Izzatbek Ulkanov <izzatbekulkanov@gmail.com>
2024-11-22 15:12:12 +0500

    update

--- apps/landing_page/urls.py
+++ apps/landing_page/urls.py
@@ -4,5 +4,7 @@ from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
-    path( "", (LandingView.as_view(template_name="index.html")), name="index",)
+    path( "", (LandingView.as_view(template_name="landing_layout/main.html")), name="index",),
+    path( "contact-me", (LandingView.as_view(template_name="landing_pages/contact_me.html")), name="contact_me",),
+    path( "partner-schools", (LandingView.as_view(template_name="landing_pages/partner_schools.html")), name="partner_schools",),
 ]
